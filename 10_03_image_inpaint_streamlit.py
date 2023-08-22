d293f1526abd08ef700c5af4d4164108e1e3cf18
Emd956 <109397313+Emd956@users.noreply.github.com>
2023-08-21 23:35:17 -0600

    Update 10_03_image_inpaint_streamlit.py

--- 10_03_image_inpaint_streamlit.py
+++ 10_03_image_inpaint_streamlit.py
@@ -57,13 +57,26 @@ if uploaded_file is not None:
         fill_color='white',
         stroke_width=stroke_width,
         stroke_color='black',
-        background_image=Image.open(uploaded_file).resize((h_, w_)),
+        background_image=Image.open(uploaded_file).resize((h, w)),  # Problema potencial
         update_streamlit=True,
         height=h_,
         width=w_,
         drawing_mode='freedraw',
         key="canvas",
     )
+
+    
+    # canvas_result = st_canvas(
+    #     fill_color='white',
+    #     stroke_width=stroke_width,
+    #     stroke_color='black',
+    #     background_image=Image.open(uploaded_file).resize((h_, w_)),
+    #     update_streamlit=True,
+    #     height=h_,
+    #     width=w_,
+    #     drawing_mode='freedraw',
+    #     key="canvas",
+    # )
     stroke = canvas_result.image_data
 
     if stroke is not None:
